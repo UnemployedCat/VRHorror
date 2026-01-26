57d57356e246d6b0763ce379d4f46a9e521c8d1e
LDKOfficial <Lasse@kjeld-gaard.dk>
2026-01-26 08:02:26 +0100

    end of friday 25/1

---
+++ VR Horror Game/Assets/Scripts/XRSocketTagInteractor.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+using UnityEngine.XR.Interaction.Toolkit.Interactors;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class XRSocketTagInteractor : XRSocketInteractor
+{
+    [SerializeField]
+    private string targetTag;
+
+    public override bool CanHover(IXRHoverInteractable interactable)
+    {
+        return base.CanHover(interactable) && interactable.transform.tag == targetTag;
+    }
+
+    public override bool CanSelect(IXRSelectInteractable interactable)
+    {
+        return base.CanSelect(interactable) && interactable.transform.tag == targetTag;
+    }
+
+}
