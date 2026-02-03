7f0cb2c39dfec4beab7cf31f106f445571518d45
LDKOfficial <Lasse@kjeld-gaard.dk>
2026-02-03 14:54:05 +0100

    Finished all the rooms and begun on footsteps

---
+++ VR Horror Game/Assets/Scripts/FootSteps.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class FootSteps : MonoBehaviour
+{
+
+    public InputActionProperty move;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
